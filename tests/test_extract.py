9a724c82c860af74fad1fa2919ec38edf9bd9b33
Miranda-vers <arthursilvamiranda@gmail.com>
2026-08-28 18:20:47 -0300

    fix: corrigir testes para passar no GitHub Actions

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,4 +1,14 @@
-﻿"""Testes para extração de dados"""
+﻿"""Testes para transformação"""
+
+def test_basic_math():
+
+
+
+cd "C:\Users\arthu\Desktop\Projeto banco AWS"
+
+# SUBSTITUIR test_extract.py
+@'
+"""Testes para extração de dados"""
 
 def test_basic_functionality():
     """Teste básico que sempre passa"""
